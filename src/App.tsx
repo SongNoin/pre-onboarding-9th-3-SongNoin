46d96f848545fb6cc8b385bcd1b5a15c65206486
SongNoin <as5427072@gmail.com>
2023-03-15 19:11:08 +0900

    style 차크라 ui 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { Main } from './pages/Main/Main';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Vite + React</h1>
-    </div>
+    <ChakraProvider>
+      <Main />
+    </ChakraProvider>
   );
 }
 
